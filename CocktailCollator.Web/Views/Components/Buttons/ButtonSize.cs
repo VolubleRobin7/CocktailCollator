0f8561b8b0eacc9403ab0d58397dcfb9aa298c44
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2025-11-09 11:42:36 +1100

    Create the button and input components

---
+++ CocktailCollator.Web/Views/Components/Buttons/ButtonSize.cs
@@ -0,0 +1,18 @@
+﻿namespace CocktailCollator.Web.Views.Components.Buttons;
+
+public class ButtonSize
+{
+    private readonly string _css;
+
+    public readonly static ButtonSize Small = new("btn-sm");
+    public readonly static ButtonSize Medium = new("");
+    public readonly static ButtonSize Large = new("btn-lg");
+
+    public string CssClass
+    {
+        get => this._css;
+    }
+
+    private ButtonSize(string css)
+        => this._css = css;
+}
---
+++ CocktailCollator.Web/Views/Components/Buttons/ButtonTheme.cs
@@ -0,0 +1,24 @@
+﻿namespace CocktailCollator.Web.Views.Components.Buttons;
+
+public class ButtonTheme
+{
+    private readonly string _css;
+
+    public readonly static ButtonTheme None = new("");
+    public readonly static ButtonTheme Primary = new("btn-primary");
+    public readonly static ButtonTheme Secondary = new("btn-secondary");
+    public readonly static ButtonTheme Success = new("btn-success");
+    public readonly static ButtonTheme Danger = new("btn-danger");
+    public readonly static ButtonTheme Warning = new("btn-warning");
+    public readonly static ButtonTheme Info = new("btn-info");
+    public readonly static ButtonTheme Light = new("btn-light");
+    public readonly static ButtonTheme Dark = new("btn-dark");
+
+    public string CssClass
+    {
+        get => this._css;
+    }
+
+    private ButtonTheme(string css)
+        => this._css = css;
+}
