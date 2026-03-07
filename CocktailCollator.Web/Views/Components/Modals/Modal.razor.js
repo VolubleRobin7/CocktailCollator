8d9f4584b282c4ea9f8aa43558247d3e6469f6f8
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-03-07 23:24:26 +1100

    Ability to call JS modal functions

---
+++ CocktailCollator.Web/Views/Components/Modals/Modal.razor.js
@@ -0,0 +1,11 @@
+﻿export function openModal(modalId) {
+    const modalElement = document.getElementById(modalId);
+    const modal = new window.bootstrap.Modal(modalElement);
+    modal.show();
+}
+
+export function closeModal(modalId) {
+    const modalElement = document.getElementById(modalId);
+    const modal = new window.bootstrap.Modal(modalElement);
+    modal.hide();
+}
