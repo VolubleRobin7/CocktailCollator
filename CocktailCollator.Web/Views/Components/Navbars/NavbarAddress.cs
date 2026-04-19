f8bd5065c4f352c67ef15370eb5deccbef76ad6e
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-04-19 18:56:07 +1000

    Self contained navbar

---
+++ CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
@@ -0,0 +1,15 @@
+﻿namespace CocktailCollator.Web.Views.Components.Navbars;
+
+public class NavbarAddress
+{
+    public readonly static NavbarAddress Recipes = new("");
+    public readonly static NavbarAddress Ingredients = new("ingredients");
+    public readonly static NavbarAddress Measurements = new("measurements");
+
+    public readonly string RelativeUri;
+
+    private NavbarAddress(string address)
+    {
+        this.RelativeUri = address;
+    }
+}
