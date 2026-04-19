cd35da9f9d109d031d11bf681475e908ab72172e
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-04-19 19:31:39 +1000

    Self Contained Navbar (#3)
    Created a self contained navigation bar that will always exist on every page, without individual implementation.

---
+++ CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
@@ -0,0 +1,17 @@
+﻿namespace CocktailCollator.Web.Views.Components.Navbars;
+
+public class NavbarAddress
+{
+    public readonly static NavbarAddress Recipes = new("", "Recipes");
+    public readonly static NavbarAddress Ingredients = new("ingredients", "Ingredients");
+    public readonly static NavbarAddress Measurements = new("measurements", "Measurements");
+
+    public readonly string DisplayName;
+    public readonly string RelativeUri;
+
+    private NavbarAddress(string address, string name)
+    {
+        this.DisplayName = name;
+        this.RelativeUri = address;
+    }
+}
