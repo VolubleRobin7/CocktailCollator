d44ee039093f70d083726f49087b023859d88399
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-04-19 19:06:57 +1000

    Definable list of pages for the nav menu

--- CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
+++ CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
@@ -2,14 +2,16 @@
 
 public class NavbarAddress
 {
-    public readonly static NavbarAddress Recipes = new("");
-    public readonly static NavbarAddress Ingredients = new("ingredients");
-    public readonly static NavbarAddress Measurements = new("measurements");
+    public readonly static NavbarAddress Recipes = new("", "Recipes");
+    public readonly static NavbarAddress Ingredients = new("ingredients", "Ingredients");
+    public readonly static NavbarAddress Measurements = new("measurements", "Measurements");
 
+    public readonly string DisplayName;
     public readonly string RelativeUri;
 
-    private NavbarAddress(string address)
+    private NavbarAddress(string address, string name)
     {
+        this.DisplayName = name;
         this.RelativeUri = address;
     }
 }
