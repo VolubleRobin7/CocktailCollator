7def914819de51d5ee0622aee8c309523ac5ff7c
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-05-12 22:23:01 +1000

    Require authentication to access some pages

--- CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
+++ CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
@@ -2,15 +2,17 @@
 
 public class NavbarAddress
 {
-    public readonly static NavbarAddress Recipes = new("", "Recipes");
-    public readonly static NavbarAddress Ingredients = new("ingredients", "Ingredients");
-    public readonly static NavbarAddress Measurements = new("measurements", "Measurements");
+    public readonly static NavbarAddress Recipes = new("", "Recipes", false);
+    public readonly static NavbarAddress Ingredients = new("ingredients", "Ingredients", true);
+    public readonly static NavbarAddress Measurements = new("measurements", "Measurements", true);
 
+    public readonly bool AuthRequired;
     public readonly string DisplayName;
     public readonly string RelativeUri;
 
-    private NavbarAddress(string address, string name)
+    private NavbarAddress(string address, string name, bool authRequired)
     {
+        this.AuthRequired = authRequired;
         this.DisplayName = name;
         this.RelativeUri = address;
     }
