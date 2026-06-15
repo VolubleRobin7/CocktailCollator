520e4c347d2d8487084ce1627776879a170c5bef
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-06-15 20:55:03 +1000

    Update Navbar to use auth policies
    AI assisted

--- CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
+++ CocktailCollator.Web/Views/Components/Navbars/NavbarAddress.cs
@@ -1,19 +1,21 @@
-﻿namespace CocktailCollator.Web.Views.Components.Navbars;
+using CocktailCollator.Web.Infrastructure.Authentication;
+
+namespace CocktailCollator.Web.Views.Components.Navbars;
 
 public class NavbarAddress
 {
-    public readonly static NavbarAddress Recipes = new("", "Recipes", false);
-    public readonly static NavbarAddress Ingredients = new("ingredients", "Ingredients", true);
-    public readonly static NavbarAddress Measurements = new("measurements", "Measurements", true);
+    public readonly static NavbarAddress Recipes = new("", "Recipes", null);
+    public readonly static NavbarAddress Ingredients = new("ingredients", "Ingredients", Policies.ViewIngredients);
+    public readonly static NavbarAddress Measurements = new("measurements", "Measurements", Policies.ViewMeasurements);
 
-    public readonly bool AuthRequired;
     public readonly string DisplayName;
+    public readonly string? Policy;
     public readonly string RelativeUri;
 
-    private NavbarAddress(string address, string name, bool authRequired)
+    private NavbarAddress(string address, string name, string? policy = null)
     {
-        this.AuthRequired = authRequired;
         this.DisplayName = name;
+        this.Policy = policy;
         this.RelativeUri = address;
     }
 }
