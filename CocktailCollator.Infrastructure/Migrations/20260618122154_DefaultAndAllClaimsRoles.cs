bd102f6f77d253c6461f5dc333a4ac097892fc18
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-06-18 22:24:25 +1000

    Default role and all claims Role entity migration

---
+++ CocktailCollator.Infrastructure/Migrations/20260618122154_DefaultAndAllClaimsRoles.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CocktailCollator.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultAndAllClaimsRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DefaultRole",
+                table: "AspNetRoles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasEveryPermissionClaim",
+                table: "AspNetRoles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultRole",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "HasEveryPermissionClaim",
+                table: "AspNetRoles");
+        }
+    }
+}
