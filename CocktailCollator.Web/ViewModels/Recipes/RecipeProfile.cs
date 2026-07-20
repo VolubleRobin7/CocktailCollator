f8e5aa6ba1ef4fe88159006d7853c85fdbe593cc
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-07-20 16:07:39 +1000

    Make sure that the file input resets correctly

--- CocktailCollator.Web/ViewModels/Recipes/RecipeProfile.cs
+++ CocktailCollator.Web/ViewModels/Recipes/RecipeProfile.cs
@@ -8,6 +8,6 @@ public class RecipeProfile : Profile
     public RecipeProfile()
     {
         _ = this.CreateMap<Recipe, RecipeViewModel>()
-            .ForMember(d => d.Images, o => o.MapFrom(s => s.Images.Select(i => i.Document)));
+            .ForMember(d => d.Images, o => o.MapFrom(s => s.Images!.Select(i => i.Document)));
     }
 }
