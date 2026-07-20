a1332519fdd75df07e5af1a69b39973cca21adec
Mitchell <100251515+VolubleRobin7@users.noreply.github.com>
2026-07-20 10:43:18 +1000

    Reduce DeleteRecipe down to one database call

--- CocktailCollator.Application/UseCases/Recipes/DeleteRecipe/DeleteRecipeInteractor.cs
+++ CocktailCollator.Application/UseCases/Recipes/DeleteRecipe/DeleteRecipeInteractor.cs
@@ -1,4 +1,4 @@
-﻿using CocktailCollator.Application.Common.Interfaces;
+using CocktailCollator.Application.Common.Interfaces;
 using CocktailCollator.Domain.Entities;
 
 namespace CocktailCollator.Application.UseCases.Recipes.DeleteRecipe;
@@ -7,16 +7,21 @@ public class DeleteRecipeInteractor(ICocktailDbContext dbContext)
 {
     public async Task Interact(DeleteRecipeInputPort inputPort, IDeleteRecipeOutputPort outputPort, CancellationToken cancellationToken)
     {
-        var _Images = dbContext.GetEntities<RecipeDocument>().Where(recipe => recipe.RecipeId == inputPort.RecipeId);
+        // Returns a tuple of Recipe and RecipeDocument
+        var _RecipeData = dbContext.GetEntities<Recipe>()
+            .Where(recipe => recipe.RecipeId == inputPort.RecipeId)
+            .Select(recipe => new { Recipe = recipe, recipe.Images })
+            .First();
 
-        foreach (var _Image in _Images)
-            dbContext.QueueRemoveDocument(_Image.DocumentId);
+        if (_RecipeData.Images is not null)
+        {
+            foreach (var _Image in _RecipeData.Images)
+                dbContext.QueueRemoveDocument(_Image.DocumentId);
+        }
 
-        var _Recipe = dbContext.GetEntities<Recipe>().First(recipe => recipe.RecipeId == inputPort.RecipeId);
-
-        dbContext.Remove(_Recipe);
+        dbContext.Remove(_RecipeData.Recipe);
 
         await dbContext.SaveChangesAsync(cancellationToken);
-        await outputPort.Success(_Recipe, cancellationToken);
+        await outputPort.Success(_RecipeData.Recipe, cancellationToken);
     }
 }
